0c325b8821fd181c68274030d6598e48fdc8ac53
Wilson Serafim <76951790+wilsonserafim@users.noreply.github.com>
2023-05-19 17:59:45 -0300

    exercicios
    a

---
+++ FatorialRecursivo/FatorialRecursivo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FatorialRecursivo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Calcular o Fatorial de qual numero? ");
+            int num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("O fatorial de " + num + " e " + Fatorial(num));
+            Console.ReadLine();
+        }
+        static int Fatorial(int num)
+        {
+            if (num <= 0) 
+            {
+                return 1;
+            
+            }
+            return num * Fatorial(num - 1);
+        }
+    }
+}
---
+++ Recursividade/Recursividade/Recursivo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursividade
+{
+    internal class Recursivo
+    {
+        public void Execultar(string mensagem, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(mensagem);
+            }
+        }
+        public void ExecultarRecursivo(string mensagem, int n)
+        {
+            if (n > 0)
+            {
+                Console.WriteLine(mensagem);
+                ExecultarRecursivo(mensagem, n - 1);
+            }
+        }
+    }
+}
